e5af8e742882c3cd47a3682751532b631525bce0
Kelvin Rosado <kelvin@localhost.localdomain>
2019-12-18 18:33:15 -0500

    New menu work

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Styling/sidebar.css'
-import Link from '@material-ui/core/Link';
 import { slide as Menu } from 'react-burger-menu'
 import HomeIcon from '@material-ui/icons/Home';
 import WorkIcon from '@material-ui/icons/Work';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import LinkIcon from '@material-ui/icons/Link';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 class SideBar extends React.Component {
     showSettings (event) {
@@ -17,13 +18,63 @@ class SideBar extends React.Component {
         "padding": "25px",
     }
 
+    buttonStyle = {
+        "color": "primary",
+        "textAlign": "center",
+        "width": "100%",
+        "padding": "25px"
+    }
+
     render () {
+
+        const classes = makeStyles(theme => ({
+            button: {
+              margin: theme.spacing(1),
+            },
+          }));
+
         return (
-            <Menu width={ '20%' } disableAutoFocus>
-                <Link style={this.linkStyle} href="/home" onClick={console.log("Pressed home")}>  Home</Link>
-                <Link style={this.linkStyle} href="/summary" onClick={console.log("Pressed summary")}>  Summary</Link>
-                <Link style={this.linkStyle} href="/resume" onClick={console.log("Pressed resume")}>  Resume</Link>
-                <Link style={this.linkStyle} href="/links" onClick={console.log("Pressed links")}>  Links</Link>
+            <Menu disableAutoFocus>
+                <Button
+                variant="contained"
+                fullWidth={true}
+                href="/home"
+                startIcon={<HomeIcon />}
+                style={this.buttonStyle}
+                className={classes.button}
+                >
+                    Home
+                </Button>
+
+                <Button
+                variant="contained"
+                fullWidth={true}
+                href="/summary"
+                startIcon={<MenuBookIcon />}
+                style={this.buttonStyle}
+                >
+                    Summary
+                </Button>
+
+                <Button
+                variant="contained"
+                fullWidth={true}
+                href="/resume"
+                startIcon={<WorkIcon />}
+                style={this.buttonStyle}
+                >
+                    Resume
+                </Button>
+
+                <Button
+                variant="contained"
+                fullWidth={true}
+                href="/links"
+                startIcon={<LinkIcon />}
+                style={this.buttonStyle}
+                >
+                    Links
+                </Button>
             </Menu>
         );
     }
