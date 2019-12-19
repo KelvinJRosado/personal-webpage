d0ef6db75a8827083ee8f04577860a7b52e85d7c
Kelvin Rosado <kelvin@localhost.localdomain>
2019-12-19 13:20:26 -0500

    Removed icons to improve appearance

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import '../Styling/sidebar.css'
 import { slide as Menu } from 'react-burger-menu'
-import HomeIcon from '@material-ui/icons/Home';
-import WorkIcon from '@material-ui/icons/Work';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import LinkIcon from '@material-ui/icons/Link';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 
 class SideBar extends React.Component {
     showSettings (event) {
@@ -16,22 +11,18 @@ class SideBar extends React.Component {
     linkStyle = {
         "color": "white",
         "padding": "25px",
+        "margin": "auto",
     }
 
     buttonStyle = {
         "color": "primary",
         "textAlign": "center",
         "width": "100%",
-        "padding": "25px"
+        "margin": "auto",
+        "padding": "20px"
     }
 
     render () {
-
-        const classes = makeStyles(theme => ({
-            button: {
-              margin: theme.spacing(1),
-            },
-          }));
 
         return (
             <Menu disableAutoFocus>
@@ -39,9 +30,7 @@ class SideBar extends React.Component {
                 variant="contained"
                 fullWidth={true}
                 href="/home"
-                startIcon={<HomeIcon />}
                 style={this.buttonStyle}
-                className={classes.button}
                 >
                     Home
                 </Button>
@@ -50,7 +39,6 @@ class SideBar extends React.Component {
                 variant="contained"
                 fullWidth={true}
                 href="/summary"
-                startIcon={<MenuBookIcon />}
                 style={this.buttonStyle}
                 >
                     Summary
@@ -60,7 +48,6 @@ class SideBar extends React.Component {
                 variant="contained"
                 fullWidth={true}
                 href="/resume"
-                startIcon={<WorkIcon />}
                 style={this.buttonStyle}
                 >
                     Resume
@@ -70,7 +57,6 @@ class SideBar extends React.Component {
                 variant="contained"
                 fullWidth={true}
                 href="/links"
-                startIcon={<LinkIcon />}
                 style={this.buttonStyle}
                 >
                     Links
